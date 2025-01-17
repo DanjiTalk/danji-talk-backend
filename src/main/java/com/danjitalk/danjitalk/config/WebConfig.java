fa21c25e54885856eda78b8f6264445385ddeb1f
yudong-yeob <maintain98@naver.com>
2025-01-18 00:10:22 +0900

    build: 프로젝트 초기 설정 - close #1

---
+++ src/main/java/com/danjitalk/danjitalk/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.danjitalk.danjitalk.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    /**
+     * CORS 설정
+     * */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+}
