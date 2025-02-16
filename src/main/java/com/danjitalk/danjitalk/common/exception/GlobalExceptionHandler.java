27eb1dde3a4fc748430f87aa07a351ea59562994
213kky <213kky@naver.com>
2025-02-16 23:42:43 +0900

    feat: 예외 처리 추가 - MethodArgumentNotValidException과 IllegalArgumentException에 대한 핸들러 구현 (#15)

--- src/main/java/com/danjitalk/danjitalk/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/danjitalk/danjitalk/common/exception/GlobalExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.danjitalk.danjitalk.common.exception;
 
 import com.danjitalk.danjitalk.common.response.ApiResponse;
+import java.util.List;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,6 +17,28 @@ public class GlobalExceptionHandler {
         return ResponseEntity
             .status(e.getCode())
             .body(ApiResponse.fail(e.getCode(), e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Void>> handleRequestValidException(MethodArgumentNotValidException e) {
+        List<String> errors = e.getBindingResult()
+            .getFieldErrors()
+            .stream()
+            .map(FieldError::getDefaultMessage)
+            .toList();
+
+        String errorMessage = String.join(" ", errors);
+
+        return ResponseEntity
+            .badRequest()
+            .body(ApiResponse.fail(HttpStatus.BAD_REQUEST.value(), errorMessage));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiResponse<Void>> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity
+            .badRequest()
+            .body(ApiResponse.fail(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
     }
 
 }
