13206a3f1da30e18fe37059944e10868db5ad840
213kky <213kky@naver.com>
2025-02-20 01:20:12 +0900

    feat: SecurityContextHolderUtil 추가 (#17)

---
+++ src/main/java/com/danjitalk/danjitalk/common/util/SecurityContextHolderUtil.java
@@ -0,0 +1,33 @@
+package com.danjitalk.danjitalk.common.util;
+
+import com.danjitalk.danjitalk.common.security.CustomMemberDetails;
+import com.danjitalk.danjitalk.domain.user.member.entity.SystemUser;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SecurityContextHolderUtil {
+
+    public static Long getMemberId() {
+        CustomMemberDetails principal = getCustomMemberDetails();
+        return principal.getUser().getSystemUserId();
+    }
+
+    public static SystemUser getSystemUser() {
+        CustomMemberDetails principal = getCustomMemberDetails();
+        return principal.getUser();
+    }
+
+    private static CustomMemberDetails getCustomMemberDetails() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new IllegalStateException("authentication is not valid");
+        }
+
+        Object principal = authentication.getPrincipal();
+        if (!(principal instanceof CustomMemberDetails)) {
+            throw new IllegalStateException("principal is not valid");
+        }
+
+        return (CustomMemberDetails) principal;
+    }
+}
