5ad975ce7299f2e0cea67f6e52982df109046124
213kky <213kky@naver.com>
2025-02-20 23:11:54 +0900

    fix: 빌드 시 테스트 실패 문제 해결 (#17)

--- src/test/java/com/danjitalk/danjitalk/DanjitalkApplicationTests.java
+++ src/test/java/com/danjitalk/danjitalk/DanjitalkApplicationTests.java
@@ -2,16 +2,12 @@ package com.danjitalk.danjitalk;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
 @SpringBootTest
-@Import(S3ClientTestConfig.class)
 class DanjitalkApplicationTests {
 
 	@Test
 	void contextLoads() {
-		System.out.println("allow-bean-definition-overriding: " +
-		System.getProperty("spring.main.allow-bean-definition-overriding"));
 	}
 
 }
