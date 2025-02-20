08d1a0b7c90f9f00a84dc3b91bcb5da611b900e4
213kky <213kky@naver.com>
2025-02-20 23:06:47 +0900

    fix: 테스트 시 H2 데이터베이스 예약어 충돌 해결 (#17)

--- src/main/java/com/danjitalk/danjitalk/domain/user/member/entity/SystemUser.java
+++ src/main/java/com/danjitalk/danjitalk/domain/user/member/entity/SystemUser.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "`system_user`")
 public class SystemUser extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
