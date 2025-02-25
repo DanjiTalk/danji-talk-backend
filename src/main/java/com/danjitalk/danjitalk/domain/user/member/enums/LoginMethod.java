15fac4f34834264d052a633d896fa369b89b096a
213kky <213kky@naver.com>
2025-02-26 03:24:33 +0900

    feat: LoginMethod enum에 네이버 로그인 추가 (#29)

--- src/main/java/com/danjitalk/danjitalk/domain/user/member/enums/LoginMethod.java
+++ src/main/java/com/danjitalk/danjitalk/domain/user/member/enums/LoginMethod.java
@@ -11,7 +11,8 @@ public enum LoginMethod {
 
     NORMAL("기본 로그인"),
     KAKAO("카카오 로그인"),
-    GOOGLE("구글 로그인");
+    GOOGLE("구글 로그인"),
+    NAVER("네이버 로그인");
 
     private final String description;
 }
