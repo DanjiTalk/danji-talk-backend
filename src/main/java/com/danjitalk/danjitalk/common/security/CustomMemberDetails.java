ddd8fb3cbcf890b54479f1fe6fac8641bc4930e6
213kky <213kky@naver.com>
2025-02-26 16:23:20 +0900

    feat: CustomMemberDetails에 OAuth2User 구현하여 OAuth 지원 추가 (#29)

--- src/main/java/com/danjitalk/danjitalk/common/security/CustomMemberDetails.java
+++ src/main/java/com/danjitalk/danjitalk/common/security/CustomMemberDetails.java
@@ -4,17 +4,37 @@ import com.danjitalk.danjitalk.domain.user.member.entity.SystemUser;
 import com.danjitalk.danjitalk.domain.user.member.enums.Role;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 @Getter
-@RequiredArgsConstructor
-public class CustomMemberDetails implements UserDetails {
+public class CustomMemberDetails implements UserDetails, OAuth2User {
 
     private final SystemUser user;
+    private Map<String, Object> attributes;
+
+    public CustomMemberDetails(SystemUser user) {
+        this.user = user;
+    }
+
+    public CustomMemberDetails(SystemUser user, Map<String, Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public <A> A getAttribute(String name) {
+        return OAuth2User.super.getAttribute(name);
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -52,5 +72,14 @@ public class CustomMemberDetails implements UserDetails {
     @Override
     public boolean isEnabled() {
         return UserDetails.super.isEnabled();
+    }
+
+    public String getEmail() {
+        return user.getLoginId();
+    }
+
+    @Override
+    public String getName() {
+        return null;
     }
 }
