2d49dced44ad689b712c35e1480eedd95ff7b5f0
yudong-yeob <maintain98@naver.com>
2025-03-04 19:58:28 +0900

    feature: 피드 아파트 entity 연관관계 매핑 및 저장 시 연관관계 세팅하는 로직 추가, Feed 생성 builder 패턴으로 변경, 부분 리팩토링 (#47)

--- src/main/java/com/danjitalk/danjitalk/domain/community/feed/entity/Feed.java
+++ src/main/java/com/danjitalk/danjitalk/domain/community/feed/entity/Feed.java
@@ -1,15 +1,13 @@
 package com.danjitalk.danjitalk.domain.community.feed.entity;
 
+import com.danjitalk.danjitalk.domain.apartment.entity.Apartment;
 import com.danjitalk.danjitalk.domain.common.entity.BaseEntity;
 import com.danjitalk.danjitalk.domain.community.comment.entity.Comment;
 import com.danjitalk.danjitalk.domain.community.feed.enums.FeedType;
 import com.danjitalk.danjitalk.domain.community.reaction.entity.Reaction;
 import com.danjitalk.danjitalk.domain.user.member.entity.Member;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -41,24 +39,41 @@ public class Feed extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     private Member member;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Apartment apartment;
+
     @OneToMany(mappedBy = "feed")
     private List<Reaction> reactionList = new ArrayList<>();
 
     @OneToMany(mappedBy = "feed")
     private List<Comment> commentList = new ArrayList<>();
 
-    public Feed(String title, String contents, FeedType feedType) {
+    // entity save 에서 사용
+    @Builder
+    public Feed(String title, String contents, FeedType feedType, String fileUrl, Member member, Apartment apartment) {
                 this.title = title;
                 this.contents = contents;
                 this.feedType = feedType;
+                this.fileUrl = fileUrl;
+                this.associateMember(member);
+                this.associateApartment(apartment);
     }
 
     /**
      * 연관관계 세팅
      * */
-    public void setMember(Member member) {
+    public void associateMember(Member member) {
+        if( member == null ) return;
+
         this.member = member;
         member.getFeedList().add(this);
+    }
+
+    public void associateApartment(Apartment apartment) {
+        if( apartment == null ) return;
+
+        this.apartment = apartment;
+        apartment.getFeedList().add(this);
     }
 
     /**
