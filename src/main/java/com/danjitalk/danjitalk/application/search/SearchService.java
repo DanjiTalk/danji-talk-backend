326ad7abbee021c26d6cd29fb107b9806b0c65f2
213kky <213kky@gmail.com>
2025-04-18 23:12:27 +0900

    feat: 아파트 검색 결과 수 캐싱 (#77)

--- src/main/java/com/danjitalk/danjitalk/application/search/SearchService.java
+++ src/main/java/com/danjitalk/danjitalk/application/search/SearchService.java
@@ -3,8 +3,10 @@ package com.danjitalk.danjitalk.application.search;
 import com.danjitalk.danjitalk.domain.search.dto.ApartmentSearchResponse;
 import com.danjitalk.danjitalk.domain.search.dto.ApartmentSearchResultResponse;
 import com.danjitalk.danjitalk.infrastructure.repository.apartment.ApartmentRepository;
+import java.time.Duration;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class SearchService {
 
     private final ApartmentRepository apartmentRepository;
+    private final RedisTemplate<String, String> redisTemplate;
 
     /**
      * 아파트 단지 검색
@@ -22,16 +25,17 @@ public class SearchService {
     @Transactional(readOnly = true)
     public ApartmentSearchResultResponse searchApartments(String keyword, Long cursor, long limit) {
         // TODO: 북마크 여부 추가하기
-        boolean isFirstPage = cursor == null;
-        Long totalCount = isFirstPage ? apartmentRepository.countByKeyword(keyword) : null;
+        String countKey = "apartment:count:" + keyword;
+        String resultCount = redisTemplate.opsForValue().get(countKey);
 
-        List<ApartmentSearchResponse> apartmentSearchResponses = apartmentRepository.findByKeywordWithCursor(keyword, cursor, limit);
-        boolean lastPage = false;
-
-        if (apartmentSearchResponses.isEmpty()) {
-            lastPage = true;
+        // TODO: 불필요한 데이터를 조회해서 캐싱되지 않기위해 keyword 걸러야할듯
+        if (resultCount == null) {
+            resultCount = String.valueOf(apartmentRepository.countByKeyword(keyword));
+            redisTemplate.opsForValue().set(countKey, resultCount, Duration.ofMinutes(10));
         }
 
-        return new ApartmentSearchResultResponse(apartmentSearchResponses, totalCount, lastPage);
+        List<ApartmentSearchResponse> apartmentSearchResponses = apartmentRepository.findByKeywordWithCursor(keyword, cursor, limit);
+        boolean lastPage = apartmentSearchResponses.isEmpty();
+        return new ApartmentSearchResultResponse(apartmentSearchResponses, Long.valueOf(resultCount), lastPage);
     }
 }
