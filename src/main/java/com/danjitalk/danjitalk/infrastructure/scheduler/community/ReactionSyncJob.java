8461b6ad6ea75499edd570f7e5cc26a15ccb898b
yudong-yeob <maintain98@naver.com>
2025-04-22 13:07:19 +0900

    feat: 좋아요 갯수 보정 작업 추가, 해당 작업은 서비스 사용에 크게 문제되지 않기에 매일 3시간씩 실행, 게시글 갯수 증가를 고려한 분할 작업 완료

---
+++ src/main/java/com/danjitalk/danjitalk/infrastructure/scheduler/community/ReactionSyncJob.java
@@ -0,0 +1,74 @@
+package com.danjitalk.danjitalk.infrastructure.scheduler.community;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jooq.DSLContext;
+import org.jooq.Field;
+import org.jooq.Query;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static com.danjitalk.danjitalk.infrastructure.jooq.table.tables.Feed.FEED;
+import static com.danjitalk.danjitalk.infrastructure.jooq.table.tables.Reaction.REACTION;
+import static org.jooq.impl.DSL.count;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class ReactionSyncJob {
+
+    private final DSLContext dslContext;
+
+    // 30초마다 실행
+    @Scheduled(cron = "0 45 0,3,6,9,12,15,18,21 * * *")
+    public void run() {
+
+        syncReactionCount();
+
+    }
+
+    private void syncReactionCount() {
+
+        Field<Integer> reactionCount = count(REACTION.ID).as("reaction_count");
+
+        List<Query> list = dslContext.select(REACTION.FEED_ID, reactionCount)
+                .from(REACTION)
+                .groupBy(REACTION.FEED_ID)
+                .stream()
+                .map(record ->
+                        (Query) dslContext.update(FEED)
+                                .set(FEED.REACTION_COUNT, record.get(reactionCount))
+                                .where(FEED.ID.eq(record.get(REACTION.FEED_ID))))
+                .toList();
+
+        if (list.isEmpty()) return;
+
+        log.info("Syncing reaction count for {} feeds", list.size());
+
+        // 500건이 넘어가면 500개씩 끊어서 사용
+        if (list.size() > 500) {
+            int BATCH_SIZE = 500;
+
+            for (int i=0; i<list.size(); i += BATCH_SIZE) {
+                List<Query> queries = list.subList(i, Math.min(i + BATCH_SIZE, list.size()));
+
+                try {
+                    dslContext.batch(queries).execute();
+                } catch (Exception e) {
+                    log.error("Batch update failed at index {}~{}, reason: {}", i, i + BATCH_SIZE, e.getMessage(), e);
+                }
+            }
+        } else {
+            try {
+                dslContext.batch(list).execute();
+            } catch (Exception e) {
+                log.error("Batch update failed, reason: {}", e.getMessage(), e);
+            }
+        }
+
+    }
+
+
+}
