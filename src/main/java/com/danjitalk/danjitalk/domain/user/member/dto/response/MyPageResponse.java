7fe407d9dd31eddeca4dd0301c94c1f1a4e02d2a
213kky <213kky@gmail.com>
2025-05-12 22:54:02 +0900

    fix: 마이페이지 조회 데이터 추가 (#89)

--- src/main/java/com/danjitalk/danjitalk/domain/user/member/dto/response/MyPageResponse.java
+++ src/main/java/com/danjitalk/danjitalk/domain/user/member/dto/response/MyPageResponse.java
@@ -1,18 +1,50 @@
 package com.danjitalk.danjitalk.domain.user.member.dto.response;
 
-public record MyPageResponse(
-    String fileId,
-    String name,
-    String nickname,
-    String email,
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import lombok.Getter;
 
-    Long memberApartmentId,
-    Long apartmentId,
-    String apartmentName,
-    String region,
-    String location,
-    String building,
-    String unit
-) {
+@Getter
+public class MyPageResponse{
+    private String fileId;
+    private String name;
+    private String nickname;
+    private String email;
+    private String phoneNumber; // 프로필 수정 부분을 위한 데이터
 
+    private Long memberApartmentId;
+    private Long apartmentId;
+    private String apartmentName;
+    private String region;
+    private String location;
+    private String building;
+    private String unit;
+
+    private LocalDate moveInDate;
+    private Integer numberOfResidents;
+    private List<String> carNumbers;
+
+    public MyPageResponse(String fileId, String name, String nickname, String email, String phoneNumber,
+        Long memberApartmentId, Long apartmentId, String apartmentName, String region, String location, String building,
+        String unit, LocalDate moveInDate, Integer numberOfResidents) {
+        this.fileId = fileId;
+        this.name = name;
+        this.nickname = nickname;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.memberApartmentId = memberApartmentId;
+        this.apartmentId = apartmentId;
+        this.apartmentName = apartmentName;
+        this.region = region;
+        this.location = location;
+        this.building = building;
+        this.unit = unit;
+        this.moveInDate = moveInDate;
+        this.numberOfResidents = numberOfResidents;
+    }
+
+    public void setCarNumbers(String carNumbers) {
+        this.carNumbers = Arrays.asList(carNumbers.split(","));
+    }
 }
--- src/main/java/com/danjitalk/danjitalk/infrastructure/repository/user/member/impl/MemberCustomRepositoryImpl.java
+++ src/main/java/com/danjitalk/danjitalk/infrastructure/repository/user/member/impl/MemberCustomRepositoryImpl.java
@@ -17,25 +17,41 @@ public class MemberCustomRepositoryImpl implements MemberCustomRepository {
 
     @Override
     public MyPageResponse getMemberInfoById(Long memberId) {
-        return queryFactory
-                .select(Projections.constructor(MyPageResponse.class,
-                    member.fileId,
-                    member.name,
-                    member.nickname,
-                    member.email,
+        MyPageResponse myPageResponse = queryFactory
+            .select(Projections.constructor(MyPageResponse.class,
+                member.fileId,
+                member.name,
+                member.nickname,
+                member.email,
+                member.phoneNumber,
 
-                    memberApartment.id,
-                    apartment.id,
-                    apartment.name,
-                    apartment.region,
-                    apartment.location,
-                    memberApartment.building,
-                    memberApartment.unit
-                ))
-                .from(member)
-                .leftJoin(memberApartment).on(member.id.eq(memberApartment.member.id))
-                .leftJoin(apartment).on(memberApartment.apartment.id.eq(apartment.id))
-                .where(member.id.eq(memberId))
-                .fetchOne();
+                memberApartment.id,
+                apartment.id,
+                apartment.name,
+                apartment.region,
+                apartment.location,
+                memberApartment.building,
+                memberApartment.unit,
+
+                memberApartment.moveInDate,
+                memberApartment.numberOfResidents
+            ))
+            .from(member)
+            .leftJoin(memberApartment).on(member.id.eq(memberApartment.member.id))
+            .leftJoin(apartment).on(memberApartment.apartment.id.eq(apartment.id))
+            .where(member.id.eq(memberId))
+            .fetchOne();
+
+        String carNumbers = queryFactory
+            .select(memberApartment.carNumbers)
+            .from(memberApartment)
+            .where(memberApartment.id.eq(myPageResponse.getMemberApartmentId()))
+            .fetchOne();
+
+        if(carNumbers != null){
+            myPageResponse.setCarNumbers(carNumbers);
+        }
+
+        return myPageResponse;
     }
 }
